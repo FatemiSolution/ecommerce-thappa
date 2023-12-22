3a946d9d20d0c370bf65bec8f26b9c91f48e1c35
FatemiSolution <shabbir.high@gmail.com>
2023-12-22 19:19:11 +0500

    added pages along with routes

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -1,10 +1,14 @@
 
 import './App.css'
-
+import {Outlet} from 'react-router-dom'
 function App() {
   
   return (
    <>
+   <h1>Hello shabbir here</h1>
+   <main>
+    <Outlet/>
+   </main>
    </>
   )
 }
--- vite-project/src/main.jsx
+++ vite-project/src/main.jsx
@@ -2,9 +2,46 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-
+import {About, Cart, Contact, ErrorPage, Home, Products, SingleProduct} from './pages';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom'
+const router = createBrowserRouter([
+  {
+    path:'/',
+    element: <App/>,
+    children:[
+      {
+        path: '/',
+        element:<Home/>,
+      },
+      {
+        path: '/about',
+        element:<About/>,
+      },
+      {
+        path: '/products',
+        element:<Products/>,
+      },
+      {
+        path: '/contact',
+        element:<Contact/>,
+      },
+      {
+        path: '/singleproduct/:id',
+        element:<SingleProduct/>,
+      },
+      {
+        path:'/cart',
+        element:<Cart/>,
+      },
+      {
+        path:'*',
+        element:<ErrorPage/>,
+      }
+    ]
+  }
+])
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
