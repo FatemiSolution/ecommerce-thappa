8d6602b1a6d8b30c61644e385a9533ec0c62ddb7
FatemiSolution <shabbir.high@gmail.com>
2024-01-03 06:15:51 +0500

    added my image for singleProduct

--- src/components/MyImages.jsx
+++ src/components/MyImages.jsx
@@ -1,13 +1,22 @@
-import React from 'react'
+import React, { useState } from 'react'
 
-function MyImages() {
+function MyImages({
+    images=[{url:''}],
+}) {
+  const [mainImage, setmainImage] = useState(images[0])
   return (
-    <div className='grid grid-cols-20'>
-        <div className='w-[30%]'>
-             <img src={''} alt='' />
+    <div className='flex'>
+        <div className='w-[30%] grid grid-rows-4'>
+          {images.map((image,index)=>(
+            <figure>
+              <img src={image.url} alt={image.filename} key={index} onClick={()=>setmainImage(image)} />
+            </figure>
+          ))}
         </div>
         <div className='w-[70%]'>
-
+        <figure>
+              <img src={mainImage.url} alt={mainImage.filename}  />
+          </figure>
         </div>
   </div>
   )
