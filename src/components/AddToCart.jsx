5171b4e602cb2a9b0c1a83069b1aa8d6d2b07c5c
FatemiSolution <shabbir.high@gmail.com>
2024-01-04 18:57:10 +0500

    added color picker

--- src/components/AddToCart.jsx
+++ src/components/AddToCart.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
-
+import React, { useState } from 'react'
+import { TiTick } from "react-icons/ti";
 function AddToCart({
     product = {},
 }) {
     const {id, colors, stock } = product;
+    const [bgcolor, setbgcolor] = useState(colors[0])
   return (
     <div>
         <p className='gap-2 flex items-center'>
             Colors:
             {colors.map((color, index) =>(
                 // {'&ensp;'}
-                <button key={index} className={'rounded-full w-5 h-5'} style={{backgroundColor:color}}>
+                <button key={index} className={`rounded-full opacity-60 hover:opacity-100 w-5 h-5 ${color === bgcolor?'opacity-100':'' }`} style={{backgroundColor:color}} onClick={()=>setbgcolor(color)}>
+                    {color === bgcolor ?<TiTick className='text-white pl-1' />: null }
+
                        
                 </button>
             ))}
